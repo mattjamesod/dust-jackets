25158e6242a0802f77599bca81f8203d8540f392
Matthew O'Donnell <matthew.james.od@gmail.com>
2022-04-17 17:17:15 +0100

    Capsule extraction view

--- Sources/DustJackets/ListEntryWithSubLine.swift
+++ Sources/DustJackets/ListEntryWithSubLine.swift
@@ -7,24 +7,67 @@ public struct ListEntryWithSubLine<Content: View>: View {
     let subline: String
     
     public var body: some View {
-        VStack(alignment: .leading) {
-            Text(headline)
-            Text(subline)
-                .font(.smallCaps(.subheadline)())
+        HStack {
+            VStack(alignment: .leading) {
+                Text(headline)
+                if subline != "" {
+                    Text(subline)
+                        .font(.smallCaps(.subheadline)())
+                }
+            }
+            Spacer()
+            content
         }
-        Spacer()
-        content
     }
     
-//    public init(headline: String, subline: String) {
-//
-//    }
+    public init(headline: String, subline: String = "", @ViewBuilder contentBuilder: () -> Content) {
+        self.headline = headline
+        self.subline = subline
+        self.content = contentBuilder()
+    }
 }
 
-//extension ListEntryWithSubLine {
-//    func freestyle() -> some View {
-//        var updatedView = self
-//        updatedView.backgroundColor = color
-//        return updatedView
-//    }
-//}
+extension ListEntryWithSubLine where Content == EmptyView {
+    public init(headline: String, subline: String = "") {
+        self.init(headline: headline, subline: subline, contentBuilder: {
+            EmptyView()
+        })
+    }
+}
+
+internal struct ListEntryWithCheckmark: View {
+    let headline: String
+    let subline: String
+    
+    var body: some View {
+        ListEntryWithSubLine(headline: headline,
+                             subline: subline) {
+            Image(systemName: "checkmark")
+        }
+    }
+}
+
+struct ListEntryWithSubLine_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 15) {
+            ListEntryWithSubLine(headline: "This entry has no subline or content")
+                .background(Color(.systemGray6))
+            ListEntryWithSubLine(headline: "This entry has no subline") {
+                Image(systemName: "square")
+            }
+            .background(Color(.systemGray6))
+            ListEntryWithSubLine(headline: "This entry has no right content",
+                                 subline: "This is an author name")
+            .background(Color(.systemGray6))
+            ListEntryWithSubLine(headline: "This is a title",
+                                 subline: "This is an author name") {
+                Image(systemName: "cross")
+            }
+            .background(Color(.systemGray6))
+            ListEntryWithCheckmark(headline: "This is from a child view",
+                                   subline: "(that always adds a tick)")
+            .background(Color(.systemGray6))
+        }
+        .padding(.horizontal, 10)
+    }
+}
