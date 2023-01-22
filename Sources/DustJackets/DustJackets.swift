350762c8158509c2d57d41ccbff872d215dd2ea9
Matthew O'Donnell <matthew.james.od@gmail.com>
2023-01-22 19:14:32 +0000

    animate unless first time

--- Sources/DustJackets/DustJackets.swift
+++ Sources/DustJackets/DustJackets.swift
@@ -5,7 +5,8 @@ public extension View {
         self.background(GeometryReader { proxy in
             Task { @MainActor in
                 guard someVariable.wrappedValue != proxy.size.height else { return }
-                someVariable.wrappedValue = proxy.size.height
+                if someVariable.wrappedValue == 0 { someVariable.wrappedValue = proxy.size.height  }
+                else { withAnimation { someVariable.wrappedValue = proxy.size.height } }
             }
             
             return Color.clear
