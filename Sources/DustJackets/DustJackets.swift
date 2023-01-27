b01023fa45d4898b142c42802d40bc8a093cb4fd
Matthew O'Donnell <matthew.james.od@gmail.com>
2023-01-27 22:52:06 +0000

    added a delay for animation

--- Sources/DustJackets/DustJackets.swift
+++ Sources/DustJackets/DustJackets.swift
@@ -3,7 +3,7 @@ import SwiftUI
 public extension View {
     func captureHeight(into someVariable: Binding<Double>) -> some View {
         self.background(GeometryReader { proxy in
-            Task { @MainActor in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
                 guard someVariable.wrappedValue != proxy.size.height else { return }
                 if someVariable.wrappedValue == 0 { someVariable.wrappedValue = proxy.size.height  }
                 else { withAnimation { someVariable.wrappedValue = proxy.size.height } }
